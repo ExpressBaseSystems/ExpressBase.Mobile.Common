d285b36c7b4cd05f59ff60bbae26fa127f0ce26b
Amalkdev <amaldev@expressbase.com>
2019-10-17 23:44:53 +0530

    eb.mob.com

---
+++ Objects/EbForm.cs
@@ -0,0 +1,13 @@
+﻿using ExpressBase.Mobile.Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ExpressBase.Mobile.Common.Objects
+{
+    public class EbForm : EbControlContainer, IEBRootObject
+    {
+       
+    }
+}
