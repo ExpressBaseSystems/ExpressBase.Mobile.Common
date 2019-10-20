839a76fcd32f290a6e1069566c4b0e5017abd507
Amalkdev <amaldev@expressbase.com>
2019-10-20 21:46:12 +0530

    mobile.comon

---
+++ Enums/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Mobile.Common.Enums
+{
+    public enum FileClass
+    {
+        files = 2,
+        image = 1
+    }
+}
