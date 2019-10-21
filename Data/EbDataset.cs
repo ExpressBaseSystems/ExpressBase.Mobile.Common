5073ad3b9e6674b83c85aed4a4436ad07c306f07
Amalkdev <amaldev@expressbase.com>
2019-10-21 22:40:11 +0530

    common

---
+++ Data/EbDataset.cs
@@ -0,0 +1,25 @@
+﻿using ExpressBase.Mobile.Common.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Mobile.Common.Data
+{
+    public class EbDataSet
+    {
+        public TableColletion Tables { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string RowNumbers { get; set; }
+
+        public EbDataSet()
+        {
+            //if (this.Tables == null) //Hack for deserialization issue Tables nullified by constructor call.. Need neater fix.
+                this.Tables = new TableColletion(this);
+        }
+    }
+}
